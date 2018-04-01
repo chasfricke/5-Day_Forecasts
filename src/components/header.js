6db42eb329516d9df4f357994781af818509d470
Chas Fricke <chasfricke@Chass-MacBook-Pro-2.local>
2018-04-01 13:31:43 -0600

    app completed

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+	render () {
+        return (
+            <div className="header-container">
+                <h1>5-DAY FORECASTS</h1>
+            </div>
+        )
+            
+	}
+}
+
+export default Header;
